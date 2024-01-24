a2cbda492f95a466828fda406790f2e90a0b3dc1
Floconi <kilian.dwwn48@gmail.com>
2024-01-24 16:03:20 +0100

    filtre terminée sauf condition et cat

--- code/index.php
+++ code/index.php
@@ -31,93 +31,95 @@ if (isset($_GET['filtre_domaine'])){
 
 
 
-  <form class = "flex " method="get" action="">
-     <?php 
-    $table_cat = "categorie" ;
-      $result = $pdo->query(" SELECT * 
-      FROM $table_cat 
-      ;");
-      $categorie = $result->fetchAll(PDO::FETCH_ASSOC); 
-  ?>
-  
-  
-  <fieldset id="categorie_filtre" class="flex flex-col">
-              <legend>
-                <?php 
-                $table_cat = ucfirst($table_cat)."s"; 
-                echo $table_cat ?>
-              </legend>
-            <?php
-            $numero_cat = 1;
-            foreach($categorie as $uneCategorie) { ?>
+  <form  method="get" action="">
+    <h2 class="flex justify-center"> Filtres sur les données <h2>
+    <div class="filtre_données flex "> 
+      <?php 
+        $table_cat = "categorie" ;
+        $result = $pdo->query(" SELECT * 
+        FROM $table_cat 
+        ;");
+        $categorie = $result->fetchAll(PDO::FETCH_ASSOC); 
+      ?>
+    
+    
+      <fieldset id="categorie_filtre" class="flex flex-col">
+        <legend>
+          <?php 
+          $table_cat = ucfirst($table_cat)."s"; 
+          echo $table_cat ?>
+        </legend>
+        <?php
+          $numero_cat = 1;
+          foreach($categorie as $uneCategorie) { ?>
+            <div class="flex"> 
+              <input name="<?php echo "categorie_n°".$numero_cat ?>" type="checkbox" id="<?php echo "categor".$numero_cat ?>" >
+              <label id="<?php echo "Label_categorie_n°".$numero_cat ?>" class="ml-2" for="<?php echo "categorie_n°".$numero_cat ?>"><?php echo $uneCategorie['nom_categorie'] ?></label>
+            </div>
+            <?php $numero_cat = $numero_cat + 1 ?>
+          <?php };
+          $numero_cat_max = $numero_cat;
+        ?>      
+      </fieldset>
 
-              <div class="flex"> 
-                <input name="<?php echo "categorie_n°".$numero_cat ?>" type="checkbox" id="<?php echo "categor".$numero_cat ?>" >
-                <label id="<?php echo "Label_categorie_n°".$numero_cat ?>" class="ml-2" for="<?php echo "categorie_n°".$numero_cat ?>"><?php echo $uneCategorie['nom_categorie'] ?></label>
+      <fieldset>
+      <legend>Condition de sélection  entre les catégories uniquement:</legend>
+        <div>
+          <input type="radio" id="ou_categorie" name="condition_categorie" value="ou" checked />
+          <label for="oucategorie">OU <br> (Ex : Je veux les résultats d'une catégorie ou alors les résultats d'une autre catégorie) </label>
+        </div>
+        <div>
+          <input type="radio" id="et_categorie" name="condition_categorie" value="et" />
+          <label for="etcategorie">ET <br> (Ex : Je veux les résultats qui ont toutes les catégories sélectionnées)</label>
+        </div>
+      </fieldset>
 
-              </div>
-              <?php $numero_cat = $numero_cat + 1 ?>
-            <?php };
-             $numero_cat_max = $numero_cat?>
-            
-          </fieldset>
+      <fieldset>
+      <legend>Condition de sélection  entre le(s) catégorie(s) et le domaine:</legend>
+        <div>
+          <input type="radio" id="ou_categorie_dom" name="condition_categorie_dom" value="ou" checked />
+          <label for="ou_categorie_dom">OU <br> (Ex : Je veux les résultats d'une catégorie ou alors les résultats d'un certain domaine) </label>
+        </div>
+        <div>
+          <input type="radio" id="et_categorie_dom" name="condition_categorie_dom" value="et" />
+          <label for="et_categorie_dom">ET <br> (Ex : Je veux les résultats d'une catégorie qui ont aussi un certain domaine)</label>
+        </div>
+      </fieldset>
 
-    <fieldset>
-    <legend>Condition de sélection  entre les catégories uniquement:</legend>
-      <div>
-        <input type="radio" id="ou_categorie" name="condition_categorie" value="ou" checked />
-        <label for="oucategorie">OU <br> (Ex : Je veux les résultats d'une catégorie ou alors les résultats d'une autre catégorie) </label>
-      </div>
-      <div>
-        <input type="radio" id="et_categorie" name="condition_categorie" value="et" />
-        <label for="etcategorie">ET <br> (Ex : Je veux les résultats qui ont toutes les catégories sélectionnées)</label>
-      </div>
-    </fieldset>
-
-    <fieldset>
-    <legend>Condition de sélection  entre le(s) catégorie(s) et le domaine:</legend>
-      <div>
-        <input type="radio" id="ou_categorie_dom" name="condition_categorie_dom" value="ou" checked />
-        <label for="ou_categorie_dom">OU <br> (Ex : Je veux les résultats d'une catégorie ou alors les résultats d'un certain domaine) </label>
-      </div>
-      <div>
-        <input type="radio" id="et_categorie_dom" name="condition_categorie_dom" value="et" />
-        <label for="et_categorie_dom">ET <br> (Ex : Je veux les résultats d'une catégorie qui ont aussi un certain domaine)</label>
-      </div>
-    </fieldset>
-  <?php 
-    $table_dom = "domaine" ;
-    $result = $pdo->query(" SELECT * 
-    FROM $table_dom 
-    ;");
-    $domaine = $result->fetchAll(PDO::FETCH_ASSOC); 
-  ?>
- <fieldset class="flex flex-col">
-              <legend>
-                    Domaine
-              </legend>
-<select id="selection_dom" name="filtre_domaine">
-  <option value="aucun" selected>Aucun filtre</option>
-  <?php
-    $numero_dom = 1;
-    foreach($domaine as $unDomaine) { ?>
-      <option id="<?php echo "domaine_n°".$numero_dom ?>" value="<?php echo $unDomaine['id_domaine'] ?>" ><?php echo $unDomaine['nom_domaine'] ?></option>
-      <?php $numero_dom = $numero_dom + 1 ?>
-      <?php } ?>
-    </select>
+      <?php 
+        $table_dom = "domaine" ;
+        $result = $pdo->query(" SELECT * 
+        FROM $table_dom 
+        ;");
+        $domaine = $result->fetchAll(PDO::FETCH_ASSOC); 
+      ?>
+      <fieldset class="flex flex-col">
+        <legend> Domaine </legend>
+        <select id="selection_dom" name="filtre_domaine">
+          <option value="aucun" selected>-- Tous les Domaines -- </option>
+          <?php
+            $numero_dom = 1;
+            foreach($domaine as $unDomaine) { ?>
+              <option id="<?php echo "domaine_n°".$numero_dom ?>" value="<?php echo $unDomaine['id_domaine'] ?>" ><?php echo $unDomaine['nom_domaine'] ?></option>
+              <?php $numero_dom = $numero_dom + 1 ?>
+            <?php } 
+          ?>
+        </select> 
+      </fieldset>
+    </div>
+    <h2> Filtre sur l'affichage Affichage 
 
 
-    <div class="flex justify-center w-48 items-center ">
-        <button type=submit >
+    
+    <button type=submit >
           Appliquer les filtres
     </button> 
-    </div>
+    
 
 
-    <div>
+    
 
-
-    <div>
+   
     
     
 
@@ -130,9 +132,9 @@ if (isset($_GET['filtre_domaine'])){
               </div>
               <?php// $numero_dom = $numero_dom + 1 ?>
             <?php// } ?>-->
-          </fieldset>
+         
 
-        </div>
+        
 
 
         
@@ -141,7 +143,7 @@ if (isset($_GET['filtre_domaine'])){
 <!-- Zone de gestion de la requete en fonction des filtres -->
 <?php 
   
-  $Requete_SQL = "SELECT  favori.id_favori,favori.libelle,GROUP_CONCAT(categorie.nom_categorie SEPARATOR "|") FROM favori "; /* Début création de la requete sql */
+ 
 
 
 
@@ -171,12 +173,15 @@ if (isset($_GET['filtre_domaine'])){
   }
 
   if(isset($_GET['condition_categorie_dom'])){
+    if ($_GET['condition_categorie_dom'] == "ou"){
     $condition_categorie_dom = "OR";
+    }else{
+      $condition_categorie_dom = "AND";
+    };
+  }
 
-  }else{
-    $condition_categorie_dom = "AND";
-  };
 
+  $Requete_SQL = "SELECT favori.id_favori,favori.libelle,favori.date_creation,favori.url, domaine.nom_domaine,GROUP_CONCAT(categorie.nom_categorie SEPARATOR ' | ') as 'liste_categorie'  FROM favori "; /* Début création de la requete sql */
   $filtre = false;
   $filtre_cat = false;
   $filtre_dom = false; 
@@ -231,10 +236,10 @@ if (isset($_GET['filtre_domaine'])){
 
   if ($filtre_cat == false && $filtre_dom == false){
 
-    $Requete_SQL = "SELECT favori.id_favori,favori.libelle,favori.date_creation,favori.url, domaine.nom_domaine,GROUP_CONCAT(categorie.nom_categorie SEPARATOR ' | ') as 'liste_categorie' FROM favori INNER JOIN favori_categorie ON favori.id_favori = favori_categorie.id_favori INNER JOIN categorie ON categorie.id_categorie = favori_categorie.id_categorie INNER JOIN domaine ON domaine.id_domaine = favori.id_dom GROUP BY favori.id_favori ORDER BY favori.id_favori ASC;";
+    $Requete_SQL = "SELECT favori.id_favori,favori.libelle,favori.date_creation,favori.url, domaine.nom_domaine,GROUP_CONCAT(categorie.nom_categorie SEPARATOR ' | ') as 'liste_categorie' FROM favori INNER JOIN favori_categorie ON favori.id_favori = favori_categorie.id_favori INNER JOIN categorie ON categorie.id_categorie = favori_categorie.id_categorie INNER JOIN domaine ON domaine.id_domaine = favori.id_dom";
 
   }
-  $Requete_SQL = $Requete_SQL." ORDER BY favori.id_favori ASC";
+  $Requete_SQL = $Requete_SQL." GROUP BY favori.id_favori ORDER BY favori.id_favori ASC";
   $Requete_SQL = $Requete_SQL." ; "; /* FIN de l'instruction SQL */
 
 
@@ -247,7 +252,7 @@ if (isset($_GET['filtre_domaine'])){
   if ($_GET['filtre_domaine'] != "aucun"){
     $Requete_SQL = $Requete_SQL."INNER JOIN domaine ON domaine.id_domaine = favori.id_dom  WHERE domaine.id_domaine = '".$_GET['filtre_domaine']."';";
   }};*/
- echo "Requete sql : ".$Requete_SQL;
+ echo "<br>Requete sql : ".$Requete_SQL;
 
   $result = $pdo->query($Requete_SQL);
   $favoris = $result->fetchAll(PDO::FETCH_ASSOC);
@@ -281,7 +286,7 @@ if (isset($_GET['filtre_domaine'])){
             <tr class="odd:bg-white even:bg-slate-50">
                 <th class="border border-black bg-gray-400 hover:bg-red-900">ID favori</th>
                 <th class="border border-black  bg-gray-400">Libellé</th>
-                <th class="border border-black  bg-gray-400">Date de création (YYYY-MM-JJ)</th>
+                <th class="border border-black  bg-gray-400">Date de création</th>
                 <th class="border border-black  bg-gray-400">Lien</th>
                 <th class="border border-black  bg-gray-400">Nom de domaine</th>
                 <th class="border border-black  bg-gray-400">Catégorie(s)</th>
@@ -289,27 +294,30 @@ if (isset($_GET['filtre_domaine'])){
             </tr>
             <?php 
                 foreach($favoris as $favori) {
-                ?>
-                <tr class="border-solid  old:bg-white even:bg-orange-200 hover:bg-green-200 ">
-                <td class=" font-bold border border-b-black  h-full"><?php echo  $favori['id_favori'] ?></td>
-                <td class="border border-b-black"><?php echo  $favori['libelle'] ?></td>
-                <td class="border border-b-black"><?php echo  $favori['date_creation'] ?></td>
-                <td class="border border-b-black"><a href="<?php echo  $favori['url']?>"><i class="fa-solid fa-arrow-up-right-from-square"></i></a></td>
-                <td class="border border-b-black"><?php echo  $favori['nom_domaine'] ?></td>
-                <td class="border border-b-black"><?php 
-                
-                $TabCatégorie = explode("|",$favori['liste_categorie']);
-                foreach ($TabCatégorie as $uneCategorie){
-                 echo  $uneCategorie."<br>";
-                }?></td>
-                <td class="flex border border-b-black">
-                  <button class="bg-orange-500 p-3 rounded mr-2" >
-                  <i class="fa-solid fa-pen-clip"></i>
-                  </button>
-                  <button class="bg-red-500 p-3 rounded" >
-                  <i class="fa-solid fa-file-circle-xmark"></i>
-                  </button>
-            </tr>
+                 
+                    ?>
+                    <tr class="border-solid  old:bg-white even:bg-orange-200 hover:bg-green-200 ">
+                    <td class=" font-bold border border-b-black  h-full"><?php echo  $favori['id_favori'] ?></td>
+                    <td class="border border-b-black"><?php echo  $favori['libelle'] ?></td>
+                    <td class="border border-b-black"><?php echo  $favori['date_creation'] ?></td>
+                    <td class="border border-b-black"><a href="<?php echo  $favori['url']?>"><i class="fa-solid fa-arrow-up-right-from-square"></i></a></td>
+                    <td class="border border-b-black"><?php echo  $favori['nom_domaine'] ?></td>
+                    <td class="border border-b-black"><?php 
+                    
+                    $TabCatégorie = explode("|",$favori['liste_categorie']);
+                    foreach ($TabCatégorie as $uneCategorie){
+                    echo  $uneCategorie."<br>";
+                    }?></td>
+                    <td class="flex border border-b-black">
+                      <button class="bg-orange-500 p-3 rounded mr-2" >
+                      <i class="fa-solid fa-pen-clip"></i>
+                      </button>
+                      <button class="bg-red-500 p-3 rounded" >
+                      <i class="fa-solid fa-file-circle-xmark"></i>
+                      </button>
+                    </tr>
+    
+
                 <?php } ?>
 
         </table> 
